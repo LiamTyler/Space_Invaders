44cfc2668cb519c36f9e574ec6b26a1a747fef7a
Liam Paul Tyler <liam.tyler@seagate.com>
2016-08-11 08:24:24 -0500

    Re-added single enemy display. No collision

--- Enemy.py
+++ Enemy.py
@@ -4,11 +4,21 @@ from Weapon import *
 from Move import Sprite
 
 class Enemy(Sprite):
-    def __init__(self, root, canvas, x = 300, y = 50, speed = 5,
-                 vel_x = 0, vel_y = 0, photo='img/gif/alien1.gif'):
+    def __init__(self, root, canvas, x = 300, y = 50, speed = 2,
+                 vel_x = 0, vel_y = 1, photo='img/gif/alien1.gif'):
         super().__init__(root, canvas, x, y, speed, vel_x, vel_y, photo)
         self.health = 10
-        self.hit
+        self.hit = 0
 
     def update(self):
-        self
+        self.health -= self.hit
+        if self.health <= 0:
+            return -1
+        # update position
+        self.x += self.speed * self.vel_x
+        self.y += self.speed * self.vel_y
+        # update weapon
+        #self.weapon.update(self.x, self.y)
+        # Redraw image
+        self.redraw()
+        return 0
--- driver.py
+++ driver.py
@@ -3,6 +3,7 @@ import time
 
 from player import Player
 from Weapon import *
+from Enemy import *
 
 class Game:
     def __init__(self):
@@ -15,12 +16,14 @@ class Game:
         self.bkgd = tk.PhotoImage(file='img/gif/space_bkgd.gif')
         self.canvas.create_image(400,425,image=self.bkgd, tags='canvas')
         self.player_1 = Player(self.root, self.canvas)
+        self.enemy = Enemy(self.root, self.canvas)
         self.update()
         self.root.mainloop()
 
     def update(self):
         self.player_1.update()
         self.player_1.collision_detection()
+        self.enemy.update()
         self.root.after(1000 // self.FPS, self.update)
 
 game = Game()
